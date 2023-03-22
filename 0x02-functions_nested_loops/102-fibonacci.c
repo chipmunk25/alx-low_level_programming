52a3a8a47747c00492e585363838ec229d1d3998
Desmond Kofi Adusei <desdhi24@gmail.com>
2023-03-22 04:41:47 -0700

    hot fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 	int n = 50;
-	int first = 1, second = 2, i, next;
+	int i;
+	long int first = 1, second = 2, next;
 
-	printf("%d, %d, ", first, second);
+	printf("%ld, %ld, ", first, second);
 
 	for (i = 3; i <= n; i++)
 	{
 		next = first + second;
-		printf("%d", next);
+		printf("%ld", next);
 
 		if (i != n)
 		{
