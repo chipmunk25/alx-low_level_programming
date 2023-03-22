a1da44bcaab1645095bc9f01ac709ffc560d984f
Desmond Kofi Adusei <desdhi24@gmail.com>
2023-03-22 04:01:39 -0700

    hot fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+
 /**
- * print_alphabet - print alphabet in lowercase
- *
+ * list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+ * Description: list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
  * Return: void
  */
 int main(void)
 {
 	int sum = 0;
-	for (int i = 0; i < 1024; i++)
+	int i;
+
+	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
