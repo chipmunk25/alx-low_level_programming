3c86b9ebdab65a288946f889895626a1f2a7b2b7
Desmond Kofi Adusei <desdhi24@gmail.com>
2023-03-22 02:36:30 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,18 +10,23 @@
 void print_times_table(int n)
 {
 	int i, j;
+	int max_chars = 4;
 
 	if (n < 0 || n > 15)
 		return;
+	while (max_chars < (n * n) / 10 + 2)
+	{
+		max_chars++;
+	}
 
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			if (j == 0)
-				printf("%*d",3, i * j);
+				printf("%*d", max_chars, i * j);
 			else
-				printf(",%*d", i * j > 9 ? 2 : 3, i * j);
+				printf(", %*d", max_chars - 2, i * j);
 		}
 		printf("\n");
 	}
