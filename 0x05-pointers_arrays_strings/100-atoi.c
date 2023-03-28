9d808a47aaeb76167c853d3555112794cda23bb5
Desmond Kofi Adusei <desdhi24@gmail.com>
2023-03-27 18:26:43 -0700

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+int main(void)
+{
+	const char* alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const int alphabet_length = 62;
+	char password[PASSWORD_LENGTH + 1] = {'\0'};
+	
+	srand(time(NULL));
+	
+	for (int i = 0; i < PASSWORD_LENGTH; i++) {
+		int random_index = rand() % alphabet_length;
+		password[i] = alphabet[random_index];
+	}
+	printf("%s\n", password);
+	return (0);
+}
