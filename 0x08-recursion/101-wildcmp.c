5ed33104c9a68251fca8546306e73eba7152958f
Desmond Kofi Adusei <desdhi24@gmail.com>
2023-04-05 17:06:22 -0700

    8. Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,95 +1,33 @@
-#include "holberton.h"
-
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
-int wildcmp(char *s1, char *s2);
+#include "main.h"
 
 /**
- * strlen_no_wilds - Returns the length of a string,
- *                   ignoring wildcard characters.
- * @str: The string to be measured.
- *
- * Return: The length.
+ * str_checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
  */
-int strlen_no_wilds(char *str)
+int str_checker(char *s1, char *s2, int i, int j)
 {
-	int len = 0, index = 0;
-
-	if (*(str + index))
-	{
-		if (*str != '*')
-			len++;
-
-		index++;
-		len += strlen_no_wilds(str + index);
-	}
-
-	return (len);
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
 }
-
 /**
- * iterate_wild - Iterates through a string located at a wildcard
- *                until it points to a non-wildcard character.
- * @wildstr: The string to be iterated through.
- */
-void iterate_wild(char **wildstr)
-{
-	if (**wildstr == '*')
-	{
-		(*wildstr)++;
-		iterate_wild(wildstr);
-	}
-}
-
-/**
- * postfix_match - Checks if a string str matches the postfix of
- *                 another string potentially containing wildcards.
- * @str: The string to be matched.
- * @postfix: The postfix.
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
  *
- * Return: If str and postfix are identical - a pointer to the null byte
- *                                            located at the end of postfix.
- *         Otherwise - a pointer to the first unmatched character in postfix.
- */
-char *postfix_match(char *str, char *postfix)
-{
-	int str_len = strlen_no_wilds(str) - 1;
-	int postfix_len = strlen_no_wilds(postfix) - 1;
-
-	if (*postfix == '*')
-		iterate_wild(&postfix);
-
-	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
-	{
-		postfix++;
-		return (postfix_match(str, postfix));
-	}
-
-	return (postfix);
-}
-
-/**
- * wildcmp - Compares two strings, considering wildcard characters.
- * @s1: The first string to be compared.
- * @s2: The second string to be compared - may contain wildcards.
- *
- * Return: If the strings can be considered identical - 1.
- *         Otherwise - 0.
+ * Return: 1 if are considered identical.
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '*')
-	{
-		iterate_wild(&s2);
-		s2 = postfix_match(s1, s2);
-	}
-
-	if (*s2 == '\0')
-		return (1);
-
-	if (*s1 != *s2)
-		return (0);
-
-	return (wildcmp(++s1, ++s2));
+	return (str_checker(s1, s2, 0, 0));
 }
